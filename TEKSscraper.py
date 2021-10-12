45d60ff1924f0d3e37cf3a387568215d540a1711
Martin Arroyo <martin@martinarroyo.dev>
2021-10-12 17:16:23 -0400

    Included POS analysis ETL
    Added additional columns to the TEKS statement tables and added data transformations that allow for the POS analysis and storage.

--- TEKSscraper.py
+++ TEKSscraper.py
@@ -1,4 +1,6 @@
 import TEKSextraction as te
+import TEKStransform as tf
+import TEKSanalyze as ta
 import pandas as pd
 import time
 import json
@@ -19,10 +21,14 @@ The general idea is to:
 -Load in our TEKS Chapters and Rules CSV
 -For each line in the CSV, we will take the chapter and rule and pass it to our extraction function
 -After the extraction happens, a new csv will be created from the extracted data. It will be named in the following structure: {Subject}-{Grade Level}
--Each line from the extracted text will be put into the new CSV as a single column
--Then the CSV will be written back to the TEKS folder
+-Each line from the extracted text will be transformed into its original structure (meaning each statement will be numbered and will include all substatements)
+-Each statement will have a list of identified Verb/Skills associated with it in a separate column as well as the associated grade and subject 
+-Then the CSV will be written back to the TEKS statements folder
+-Additionally, the individual TEKS statements will be transformed into a whole page (the entire set of statements for a chapter(subject) and rule(grade))
+-These full statements will be analyzed and the verbs associated with these statements will be counted and put into an analysis file
+-The analysis file names will follow this structure: {Subject}-{Grade}_verb_frequency
+-After all of the individual files have been written, master files will be created that are a union of all the tables/files for the statements and analysis sets
 """
-
 for index,row in teks_ch_rules.iterrows():
     # Extract row information
     subject = row['Subject']
@@ -32,8 +38,30 @@ for index,row in teks_ch_rules.iterrows():
     # Create DataFrame by extracting TEKS data
     teks_dir = paths['path_to_teks_dir']
     csv_name = f"{subject}-{grade}.csv"
-    df = pd.DataFrame(te.extract_teks(chapter,rule), columns=['Statements'])
+    teks_statements = tf.transform(te.extract_teks(chapter,rule))
+    teks_statement_verbs = ta.get_verbs(teks_statements)
+    teks_statements_table = pd.DataFrame({'Statement':teks_statements, 'Verbs/Skills':teks_statement_verbs,'Grade': grade,'Subject':subject})
     # Write the DataFrame to our folder as a CSV
-    df.to_csv(teks_dir + csv_name)
+    teks_statements_table.to_csv(teks_dir + csv_name)
+    # Create analysis tables for each TEKS page
+    # Transform collection of statements into a full page
+    teks_page_verbs = ta.get_verbs(tf.transform_to_page(teks_statements), whole_page=True)
+    # Get the verbs and frequencies of each
+    verb,frequency = ta.get_verbs_and_counts(teks_page_verbs)
+    teks_analysis_table = pd.DataFrame({'Verb/Skill':verb,'Frequency':frequency,'Grade': grade,'Subject':subject})
+    # Naming for the analysis tables
+    csv_analysis_name = f"{subject}-{grade}_verb_frequency.csv"
+    # Write the table to our director
+    teks_analysis_table.to_csv(paths['path_to_teks_analysis']+csv_analysis_name)
     # Create a 10 second delay to avoid multiple server calls back-to-back
     time.sleep(10)
+
+
+"""
+    After all of our data is extracted into individual files and folders,
+    create master files for our statements and analysis files
+"""
+# Analysis folder
+tf.write_to_master(paths["path_to_teks_analysis"])
+# Statements folder
+tf.write_to_master(paths["path_to_teks_dir"])
