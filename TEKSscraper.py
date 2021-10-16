76b78c921da14e305ee56de0f38b411dc852dd47
Martin Arroyo <martin@martinarroyo.dev>
2021-10-16 02:14:31 -0400

    Added guard to script

--- TEKSscraper.py
+++ TEKSscraper.py
@@ -29,39 +29,42 @@ The general idea is to:
 -The analysis file names will follow this structure: {Subject}-{Grade}_verb_frequency
 -After all of the individual files have been written, master files will be created that are a union of all the tables/files for the statements and analysis sets
 """
-for index,row in teks_ch_rules.iterrows():
-    # Extract row information
-    subject = row['Subject']
-    grade = row['Grade Level Indicated']
-    chapter = row['Chapter']
-    rule = row['Rule']
-    # Create DataFrame by extracting TEKS data
-    teks_dir = paths['path_to_teks_dir']
-    csv_name = f"{subject}-{grade}.csv"
-    teks_statements = tf.transform(te.extract_teks(chapter,rule))
-    teks_statement_verbs = ta.get_verbs(teks_statements)
-    teks_statements_table = pd.DataFrame({'Statement':teks_statements, 'Verbs/Skills':teks_statement_verbs,'Grade': grade,'Subject':subject})
-    # Write the DataFrame to our folder as a CSV
-    teks_statements_table.to_csv(teks_dir + csv_name)
-    # Create analysis tables for each TEKS page
-    # Transform collection of statements into a full page
-    teks_page_verbs = ta.get_verbs(tf.transform_to_page(teks_statements), whole_page=True)
-    # Get the verbs and frequencies of each
-    verb,frequency = ta.get_verbs_and_counts(teks_page_verbs)
-    teks_analysis_table = pd.DataFrame({'Verb/Skill':verb,'Frequency':frequency,'Grade': grade,'Subject':subject})
-    # Naming for the analysis tables
-    csv_analysis_name = f"{subject}-{grade}_verb_frequency.csv"
-    # Write the table to our director
-    teks_analysis_table.to_csv(paths['path_to_teks_analysis']+csv_analysis_name)
-    # Create a 10 second delay to avoid multiple server calls back-to-back
-    time.sleep(10)
 
 
-"""
-    After all of our data is extracted into individual files and folders,
-    create master files for our statements and analysis files
-"""
-# Analysis folder
-tf.write_to_master(paths["path_to_teks_analysis"])
-# Statements folder
-tf.write_to_master(paths["path_to_teks_dir"])
+if __name__ == '__main__':
+    for index,row in teks_ch_rules.iterrows():
+        # Extract row information
+        subject = row['Subject']
+        grade = row['Grade Level Indicated']
+        chapter = row['Chapter']
+        rule = row['Rule']
+        # Create DataFrame by extracting TEKS data
+        teks_dir = paths['path_to_teks_dir']
+        csv_name = f"{subject}-{grade}.csv"
+        teks_statements = tf.transform(te.extract_teks(chapter,rule))
+        teks_statement_verbs = ta.get_verbs(teks_statements)
+        teks_statements_table = pd.DataFrame({'Statement':teks_statements, 'Verbs/Skills':teks_statement_verbs,'Grade': grade,'Subject':subject})
+        # Write the DataFrame to our folder as a CSV
+        teks_statements_table.to_csv(teks_dir + csv_name)
+        # Create analysis tables for each TEKS page
+        # Transform collection of statements into a full page
+        teks_page_verbs = ta.get_verbs(tf.transform_to_page(teks_statements), whole_page=True)
+        # Get the verbs and frequencies of each
+        verb,frequency = ta.get_verbs_and_counts(teks_page_verbs)
+        teks_analysis_table = pd.DataFrame({'Verb/Skill':verb,'Frequency':frequency,'Grade': grade,'Subject':subject})
+        # Naming for the analysis tables
+        csv_analysis_name = f"{subject}-{grade}_verb_frequency.csv"
+        # Write the table to our director
+        teks_analysis_table.to_csv(paths['path_to_teks_analysis']+csv_analysis_name)
+        # Create a 10 second delay to avoid multiple server calls back-to-back
+        time.sleep(10)
+
+
+    """
+        After all of our data is extracted into individual files and folders,
+        create master files for our statements and analysis files
+    """
+    # Analysis folder
+    tf.write_to_master(paths["path_to_teks_analysis"])
+    # Statements folder
+    tf.write_to_master(paths["path_to_teks_dir"])
